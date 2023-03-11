fea7780a424b23ffcc61f5a5f25e1d65f0787dc7
Brian GL <brian.gaytan4368@alumnos.udg.mx>
2023-03-11 12:33:34 -0600

    Correcciones generales para deploy

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/golang-sql/sqlexp v0.1.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1
 	github.com/microsoft/go-mssqldb v0.17.0 // indirect
 	golang.org/x/crypto v0.0.0-20221005025214-4161e89ecf1b // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,124 @@
 package main
 
 import (
-	"calculator-courseroom/controllers"
+	"bytes"
+	"calculator-courseroom/async"
+	"calculator-courseroom/infrastructure"
+	"calculator-courseroom/models"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net"
 	"net/http"
 	"net/rpc"
 	"os"
 	"time"
+
+	"gorm.io/driver/sqlserver"
+	"gorm.io/gorm"
 )
+
+type RPCServer struct {
+	DB           *gorm.DB
+	BRIDGE       *string
+	SECRET_TOKEN string
+}
+
+func NewRPCServer() *RPCServer {
+
+	// Cargar archivo .env
+	//godotenv.Load(".env")
+
+	//Cargar variables:
+
+	server := os.Getenv("SERVER")
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	databaseName := os.Getenv("DATABASE")
+	SECRET_TOKEN := os.Getenv("SECRET_TOKEN")
+	BRIDGE := os.Getenv("BRIDGE")
+
+	dsn := "sqlserver://" + user + ":" + password + "@" + server + "?database=" + databaseName
+
+	db, _ := gorm.Open(sqlserver.Open(dsn), &gorm.Config{
+		SkipDefaultTransaction: true,
+		PrepareStmt:            true,
+	})
+
+	return &RPCServer{DB: db, BRIDGE: &BRIDGE, SECRET_TOKEN: SECRET_TOKEN}
+}
+
+func (server *RPCServer) Calificacion(model *models.TareaCalificacionInputModel, reply *any) error {
+
+	// Validar que el token sea el correcto:
+
+	if server.SECRET_TOKEN == model.SECRET_TOKEN {
+
+		future := async.Exec(func() interface{} {
+			return infrastructure.InformacionDesempenoUsuarioGetAsync(server.DB, model)
+		})
+
+		//Obtener las estadisticas iniciales del usuario:
+		estadisticasUsuario := future.Await().([]models.CalculatorInformacionDesempenoObtenerEntity)
+
+		if estadisticasUsuario != nil {
+			if len(estadisticasUsuario) > 0 {
+
+				var array_calificaciones_x []float32
+				var array_calificaciones_y []float32
+
+				for _, value := range estadisticasUsuario {
+
+					array_calificaciones_x = append(array_calificaciones_x, float32(value.Indice))
+					array_calificaciones_y = append(array_calificaciones_y, value.Resultado)
+				}
+
+				//Llamar script de matlab:
+
+				modelBridge := models.BridgeModel{
+					X: array_calificaciones_x,
+					Y: array_calificaciones_y,
+				}
+
+				jsonValue, _ := json.Marshal(modelBridge)
+
+				//Llamar al bridge:
+				resp, err := http.Post(*server.BRIDGE+"RegresionPolinomial", "application/json", bytes.NewBuffer(jsonValue))
+
+				if err == nil {
+
+					defer resp.Body.Close()
+
+					body, err := io.ReadAll(resp.Body)
+
+					if err == nil {
+
+						//Obtener respuesta del bride como json:
+						var bridgeResponse models.BridgeEntity
+						err = json.Unmarshal(body, &bridgeResponse)
+
+						if err == nil {
+
+							if bridgeResponse.Codigo > 0 {
+
+								//Si la respuesta es correcta actualizar el desempeño por lo que nos regresa el algoritmo inteligente:
+								modelDesempenoActualizar := models.UsuarioDesempenoActualizarInputModel{
+									IdDesempeno:                 model.IdDesempeno,
+									PrediccionCalificacionCurso: &bridgeResponse.Resultado,
+									RumboCalificacionCurso:      &bridgeResponse.Mensaje,
+								}
+
+								infrastructure.UsuarioDesempenoActualizarPutAsync(server.DB, &modelDesempenoActualizar)
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return nil
+}
 
 func main() {
 	fmt.Print("\033[H\033[2J")
@@ -19,13 +129,10 @@ func main() {
 
 func Servering() {
 
-	rpcServer := controllers.NewRPCServer()
+	rpcServer := NewRPCServer()
 	rpc.Register(rpcServer)
 
-	// Register a HTTP handler
-	rpc.HandleHTTP()
-
-	server_rpc, error := net.Listen("tcp", ":1414")
+	server_rpc, error := net.Listen("tcp", ":2023")
 	if error != nil {
 		fmt.Println("Found Error : ", error.Error())
 		server_rpc.Close()
@@ -34,16 +141,20 @@ func Servering() {
 
 	fmt.Println("\nCourseRoom Calculator Opened At " + time.Now().Format("Monday 2006-01-02 15:04"))
 
+	//Home page:
+	//http.HandleFunc("/rpc", Index)
+
 	defer server_rpc.Close()
 
-	//Home page:
-	http.HandleFunc("/rpc", Index)
-
-	// Start accept incoming HTTP connections:
-	error = http.Serve(server_rpc, nil)
-	if error != nil {
-		fmt.Println("Found Error : ", error.Error())
-		return
+	for {
+		fmt.Println("Listening...")
+		client, err_or := server_rpc.Accept()
+		if err_or != nil {
+			fmt.Println("Found Error: ", err_or)
+			continue
+		}
+		fmt.Println("Connected Client With Address:", client.RemoteAddr())
+		go rpc.ServeConn(client)
 	}
 }
 
